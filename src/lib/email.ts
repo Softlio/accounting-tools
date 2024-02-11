82c12d755928f161e1a4c07d68a3af78dd487d2f
Softlio <cjsilvamon@gmail.com>
2024-02-11 12:29:30 +0100

    changed email settings

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -3,6 +3,7 @@ import { createTransport } from "nodemailer";
 export const transporter = createTransport({
   host: process.env.SMTP_HOST,
   from: process.env.SMTP_FROM,
+  port: parseInt(process.env.SMTP_PORT || "587"),
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
