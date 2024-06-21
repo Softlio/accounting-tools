36ec93fb264bd58885bf4a99e76ece92353aef1c
Softlio <cjsilvamon@gmail.com>
2024-06-21 09:35:29 +0200

    added mobile icon

--- src/components/landing/Navbar.tsx
+++ src/components/landing/Navbar.tsx
@@ -1,20 +1,25 @@
 import getTranslation from '@/translations/getTranslation'
+import { User } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { Button } from '../ui/button'
 
 const Navbar = () => {
     return (
-        <div className='w-full sticky top-0 z-50 bg-theme-light shadow-md py-4'>
-            <nav className='container mx-auto flex justify-between items-center max-sm:px-2 h-14'>
+        <div className='sticky top-0 z-50 w-full py-4 shadow-md bg-theme-light'>
+            <nav className='container flex items-center justify-between mx-auto max-sm:px-2 h-14'>
                 <a href="https://femfinancialservices.nl/">
                     <Image src="/images/seal-logo.png" alt="FEM Financial Services" width={400} height={600} className='mx-4 max-w-[170px] -m-36 absolute' />
                 </a>
-                <Button size={"lg"} asChild className='text-xl px-10 py-6 bg-theme-secondary transition-colors duration-300 font-serif font-bold max-sm:hidden'>
+                <Button size={"lg"} asChild className='px-10 py-6 font-serif text-xl font-bold transition-colors duration-300 bg-theme-secondary max-sm:hidden'>
                     <Link href='/login'>
                         {getTranslation.landingPage.hero.loginButton}
                     </Link>
                 </Button>
+                <Button size={'lg'} className='hidden w-10 h-10 p-1 mr-3 bg-transparent max-sm:flex'>
+                    <User size={32} className='text-theme-secondary' />
+                </Button>
+
             </nav>
         </div>
     )
