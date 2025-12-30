aa3cf5f6fe1fbfec9fde3f7b0683b36236e5ebe2
Softlio <cjsilvamon@gmail.com>
2025-12-30 12:56:02 +0100

    updated dependencies

--- src/app/api/download/income-tax/[id]/route.tsx
+++ src/app/api/download/income-tax/[id]/route.tsx
@@ -5,12 +5,16 @@ import IncomeTaxPdf from "@/pdfs/IncomeTaxPdf";
 import { renderToBuffer } from "@joshuajaco/react-pdf-renderer-bundled";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(request: NextRequest, { params: { id } }: {
-  params: {
-    id: string;
-  };
-}) {
-
+export async function GET(
+  request: NextRequest,
+  {
+    params: { id },
+  }: {
+    params: {
+      id: string;
+    };
+  }
+) {
   if (!id) {
     Logger.error("downloadIncomeTax", "No id provided");
     return new NextResponse("No id provided", { status: 400 });
@@ -45,23 +49,30 @@ export async function GET(request: NextRequest, { params: { id } }: {
       salaried: incomeTaxCalculation.hadSalariedEmployment,
       annual_salary: incomeTaxCalculation.annualIncome ?? 0,
       tax_withheld: incomeTaxCalculation.taxWithheld ?? 0,
-    }
+    };
 
     const result = calculateIncomeTax(input);
 
     const buffer = await renderToBuffer(
-      <IncomeTaxPdf values={result} input={input} id={id} createdAt={incomeTaxCalculation.payment.paidAt ?? new Date()} />,
+      <IncomeTaxPdf
+        values={result}
+        input={input}
+        id={id}
+        createdAt={incomeTaxCalculation.payment.paidAt ?? new Date()}
+      />
     );
 
-    return new NextResponse(buffer, {
+    return new NextResponse(new Uint8Array(buffer), {
       headers: {
         "Content-Type": "application/pdf",
         "Content-Disposition": `attachment; filename="income_tax_${incomeTaxCalculation.id}.pdf"`,
       },
     });
   } catch (error) {
-    Logger.error("downloadIncomeTax", "Error while generating income tax pdf: " + JSON.stringify(error));
+    Logger.error(
+      "downloadIncomeTax",
+      "Error while generating income tax pdf: " + JSON.stringify(error)
+    );
     return new NextResponse("Internal server error", { status: 500 });
   }
 }
-
